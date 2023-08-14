b991dfc81f7a7d8f2695b57f3ff72fa07f3f2ea4
tejasprabhune <tejas.prabhune@gmail.com>
2023-08-13 17:26:55 -0700

    Add video options

--- website2/js/shottracer.js
+++ website2/js/shottracer.js
@@ -1,27 +1,16 @@
-const getValuesFromInputs = () => {
-    const uploadTrainee = document.getElementById("upload-trainee-input").files[0];
-
-    console.log(uploadTrainee);
-    return uploadTrainee;
-}
-
-const convertInputValues = () => {
-    const uploadTrainee = getValuesFromInputs();
-
-    const uploadTraineeURL = URL.createObjectURL(uploadTrainee);
-
-    return uploadTraineeURL;
-}
-
-const addInputToCard = () => {
-    const traineeURL = convertInputValues();
-
-    document.getElementById("trainee-video").setAttribute('src', traineeURL);
-}
-
-
 const traineeInput = document.getElementById("upload-trainee-input");
+const traineeSource = document.getElementById("trainee-source");
+const traineeVideo = document.getElementById("trainee-video");
+
+const coachSource = document.getElementById("coach-source");
+const coachVideo = document.getElementById("coach-video");
+
+const showTennisServe = document.getElementById("show-serve-button");
+const showTableTennis = document.getElementById("show-tt-button");
+
 traineeInput.addEventListener("change", handleTraineeInput, false);
+showTennisServe.addEventListener("click", handleShowTennisServe, false);
+showTableTennis.addEventListener("click", handleShowTableTennis, false);
 
 function handleTraineeInput() {
     const traineeFile = this.files[0];
@@ -31,11 +20,29 @@ function handleTraineeInput() {
     console.log(uploadTraineeURL);
     console.log(traineeFile["type"]);
 
-    document.getElementById("trainee-video").pause();
-    document.getElementById("trainee-source").setAttribute('src', uploadTraineeURL);
-    document.getElementById("trainee-source").setAttribute('type', traineeFile["type"]);
+    traineeVideo.pause();
+    traineeSource.setAttribute('src', uploadTraineeURL);
+    traineeSource.setAttribute('type', traineeFile["type"]);
 
-    //document.getElementById("trainee-video").setAttribute("visibility", "visible");
-    document.getElementById("trainee-video").load();
-    document.getElementById("trainee-video").play();
+    traineeVideo.setAttribute("controls", "controls");
+    traineeVideo.load();
+    traineeVideo.play();
+}
+
+function handleShowTennisServe() {
+    handleShowCoachVideo("../videos/TennisSwing.mp4");
+}
+
+function handleShowTableTennis() {
+    handleShowCoachVideo("../videos/TableTennis.mp4");
+}
+
+function handleShowCoachVideo(video_url) {
+    coachVideo.pause();
+    coachSource.setAttribute('src', video_url);
+    coachSource.setAttribute("type", "video/mp4");
+
+    coachSource.setAttribute("controls", "controls");
+    coachVideo.load();
+    coachVideo.play();
 }
